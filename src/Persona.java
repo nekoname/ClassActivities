bdaad8d4829acd34f413dae283319aab0d863980
nekoname <ltorrente@nigul.cide.es>
2021-11-16 16:37:34 +0100

    Initial commit

---
+++ src/Persona.java
@@ -0,0 +1,25 @@
+public class Persona {
+    /**
+     * 1.Atributos
+     * 2.metodos
+     * 3.contructor
+     */
+    private char sexo;
+    private int edad;
+    private String DNI;
+    private double peso;
+    private double altura;
+    public Persona(){
+        this.sexo= 'H';
+        //this.edad;
+    }public Persona(String nom, int edad, char sexo,String dni, double peso, double altura){
+        this.nom = nom;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo= sexo;
+        this.peso = peso;
+        this.altura = altura;
+    public static void main(String[] args) {
+
+    }
+}
